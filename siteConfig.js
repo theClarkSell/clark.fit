33b7f9d2906729589524432165cb2e8a10403b05
Clark Sell <clark.sell@csell.net>
2020-03-29 12:21:23 -0500

    inital site drop

---
+++ siteConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "Clark Sell",
+  shortName: "clark.fit",
+  description:
+    "A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery",
+  url: "https://clark.fit", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Clark Sell",
+  twitter: "@csell5",
+}
