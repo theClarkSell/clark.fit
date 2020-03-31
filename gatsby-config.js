1189b0eeb747171446a605c6876e47b93a721a07
Clark Sell <clark.sell@csell.net>
2020-03-31 06:22:07 -0500

    adds ga id

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: ``,
+        trackingId: `UA-21705613-13`,
       },
     },
     `gatsby-plugin-feed`,
